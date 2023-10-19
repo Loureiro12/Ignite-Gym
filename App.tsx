2ec7e5ab9fc908c6c6ff54b3123c5e49883935ee
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-10-19 02:01:25 -0300

    feat: component-library

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { StatusBar, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 
 import {
   useFonts,
@@ -6,24 +7,22 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from "@components/Loading";
+import { SignUp } from "@screens/SignUp";
+
+import { THEME } from "./src/theme";
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World</Text> : <View />}
-    </View>
+      {fontsLoaded ? <SignUp /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
